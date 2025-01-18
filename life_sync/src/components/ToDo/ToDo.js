d5a77cdd7bd6ce1ac8479efc2c230569ef3747e1
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2025-01-18 20:58:59 +0300

    Add cheched state for list items

--- life_sync/src/components/ToDo/ToDo.js
+++ life_sync/src/components/ToDo/ToDo.js
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ToDo.module.css";
 import ToDoTitle from "./ToDoTitle/ToDoTitle";
 import ToDoForm from "./ToDoForm/ToDoForm";
 import ToDoList from "./ToDoList/ToDoList";
 
 export default function ToDo() {
-  const title = "ToDo";
+  const title = "To-Do List";
 
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+  const [checked, setChecked] = useState([]);
+  useEffect(() => {
+    console.log(checked);
+  }, [checked]);
+  function handleChecked(index) {
+    const updatedCheckedState = checked.map((item, pos) =>
+      pos === index ? !item : item
+    );
+    console.log(index);
+    setChecked(updatedCheckedState);
+  }
 
   function handleChange(e) {
     setInputValue(e.target.value);
@@ -16,12 +27,17 @@ export default function ToDo() {
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...todos, inputValue]);
+    setChecked([...checked, false]);
     setInputValue("");
   }
   function handleDelete(index) {
     let newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
+
+    let newChecked = [...checked];
+    newChecked.splice(index, 1);
+    setChecked(newChecked);
   }
 
   return (
@@ -32,7 +48,12 @@ export default function ToDo() {
         handleChange={handleChange}
         handleSubmit={handleSubmit}
       />
-      <ToDoList todos={todos} handleDelete={handleDelete} />
+      <ToDoList
+        todos={todos}
+        handleDelete={handleDelete}
+        handleChecked={handleChecked}
+        checked={checked}
+      />
     </div>
   );
 }
