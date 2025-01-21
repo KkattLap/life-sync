58ed4d31cc3f16930117b6be612b9804caf2b2ac
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2025-01-21 17:08:07 +0300

    Add react-router

--- life_sync/src/index.js
+++ life_sync/src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
