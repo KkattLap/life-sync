96223c96da124c5127c230393fbee8a0301664ec
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2025-01-26 13:18:03 +0300

    Add routing

--- life_sync/src/index.js
+++ life_sync/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
+import Home from "./components/Home/Home";
+import Calendar from "./components/Calendar/Calendar";
+import ToDo from "./components/ToDo/ToDo";
 import "./index.css";
 
 const root = document.getElementById("root");
@@ -9,7 +12,30 @@ const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route
+        path="/"
+        element={
+          <App>
+            <Home />
+          </App>
+        }
+      />
+      <Route
+        path="/calendar"
+        element={
+          <App>
+            <Calendar />
+          </App>
+        }
+      />
+      <Route
+        path="/todo"
+        element={
+          <App>
+            <ToDo />
+          </App>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
