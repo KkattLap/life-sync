b19c11b50f3224a3d99286ef324db901bfef3526
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2025-02-18 22:13:35 +0300

    Redesign Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,46 +1,18 @@
 import styles from "./Home.module.css";
 import "../../fonts.css";
 import timeManagmentImg from "./timeManagment.jpg";
+import DiaryButton from "./DiaryButton/DiaryButton";
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <div className={styles.mainBlock}>
         <div className={styles.textBlock}>
-          <h1 className={`${styles.textHeader} textHeader_font_48`}>
-            <span
-              className={`${styles.textHeader__word1} 
-              ${styles.textHeader__word1_rotate} 
-              ${styles.textHeader__word1_position}`}
-            >
-              Синхронизируй
-            </span>
-            <span
-              className={`${styles.textHeader__word2} 
-              ${styles.textHeader__word2_rotate}  
-              ${styles.textHeader__word2_position}`}
-            >
-              Свой
-            </span>
-            <span
-              className={`${styles.textHeader__word3} 
-                ${styles.textHeader__word3_rotate}  
-                ${styles.textHeader__word3_position} 
-                ${styles.textHeader__word3_red}`}
-            >
-              Хаос
-            </span>
-            <br />
-            <div className={styles.otherHeaderWords}>
-              <span className={styles.otherHeaderWords__word_noShrink}>
-                Обрети Свой
-              </span>
-              <span
-                className={`${styles.otherHeaderWords__word3} ${styles.otherHeaderWords__word3_accentColor}`}
-              >
-                Порядок
-              </span>
-            </div>
+          <h1 className={`${styles.textHeader} textHeader_font_64`}>
+            Синхронизируй
+            <br /> Свой Хаос
+            <br /> И Обрети
+            <br /> Свой Порядок
           </h1>
           <p className={`${styles.textBlock__p} textBlock__p_font_16`}>
             Открой для себя идеальный помощник для организации своих мыслей и
@@ -48,6 +20,7 @@ export default function Home() {
             фокусируйся на главном, чтобы каждый день приближаться к своим
             целям!
           </p>
+          <DiaryButton></DiaryButton>
         </div>
         <div className={styles.imgContainer}>
           <img
